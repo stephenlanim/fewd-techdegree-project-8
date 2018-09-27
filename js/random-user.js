01f183bb974e1d60ac59afbf5efd980d3946b3b7
Stephen Anim <stephen.lloyd.anim@gmail.com>
2018-09-27 17:20:03 -0400

    Created working fetch callbacks for one user.
    Successfully got the fetch function to upload content in all the appropriate fields on the page for a single user in the directory.
    
    Created multiple functions for this.

--- js/random-user.js
+++ js/random-user.js
@@ -1,30 +1,16 @@
 /* ======================================
     Random User Generator API
    ====================================== */
-
-
-// Fetch JSON data for 12 users from Random User API
-fetchData('https://randomuser.me/api/?results=12')
-//
-  .then(data => {
-    console.log(data);
-  });
-
-// Function to insert random user data into directory-cards and modal-cards
-function insertRandomUsers(jsonObject) {
-  //For each random user...
-
-
-}
+// $(document).ready(function () {
 
 // Get directory-card targets
-const $dcImage = $('.directory-card .avatar');
+const $dcImage = $('.directory-card .photo');
 const $dcName = $('.directory-card .name');
 const $dcEmail = $('.directory-card .email');
 const $dcCity = $('.directory-card .city');
 
 // Get modal-card targets
-const $mcImage = $('.modal-card .avatar');
+const $mcImage = $('.modal-card .photo');
 const $mcName = $('.modal-card .name');
 const $mcEmail = $('.modal-card .email');
 const $mcCity = $('.modal-card .city');
@@ -32,35 +18,181 @@ const $mcPhone = $('.modal-card .phone');
 const $mcAddress = $('.modal-card .address');
 const $mcBirthday = $('.modal-card .birthday');
 
-// Function for inserting basic employee info
-  // variables to pass in: image, name, email, city
-  // ... I may also need to pass in the object properties from the JSON object, perhaps as "sources".
-function insertBasicInfo (sourceImg, sourceEmail, sourceCity, imageTarget, emailTarget, cityTarget) {
+// Initialize variables for Fetch data properties
+  // Note: Initializing them inside of any functions only made them accessible to that function and not any nested functions, despitethe fact that nested functions should have access to variables from parent functions. The solution was to initialize these variables on a global scope and update their values after the data was fetched.
+let sourceImage;
+let sourceName;
+let sourceEmail;
+let sourceCity;
+let sourcePhone;
+let parsedPhone;
+let sourceAddress;
+let sourceBirthday;
 
-  // Insert employee image
-  imageTarget.
-  // Insert First and Name
+// Fetch JSON data for 12 users from Random User API
+fetchData('https://randomuser.me/api/?results=1&nat=us')
+//
+  .then(data => {
 
-  // Insert email
 
-  // Insert city
+    // console.log(data);
+    console.log(data.results);
+    // console.log(`${data.results[0].name.first} ${data.results[0].name.last}`);
+    // console.log(capitalize(data.results[0].name.first));
+    // console.log(capitalize(data.results[0].location.street));
+    // console.log(data.results[0].email);
+    // insertRandomUsers(data);
+    data.results.map( user => {
+      // Get properties from user JSON Object
+      sourceImage = `${user.picture.large}`;
+      sourceName = `${user.name.first} ${user.name.last}`;
+      sourceEmail = user.email;
+      sourceCity = user.location.city;
+      sourcePhone = user.phone;
+      parsedPhone = `tel:+1${parsePhoneNumber(user.phone)}`;
+      sourceAddress = `${user.location.street}, ${user.location.city}, ${user.location.state} ${user.location.postcode}`;
+      sourceBirthday = formatBirthday(user.dob.date);
+
+      insertRandomUsers();
+    }
+
+    );
+  });
+
+// I need to test if JSON data can be passed down to nested functions, since that will let me know if I need to pass variables into nested functions or not. I'm guessing I don't since I've used "this" or e.target in nested functions before.
+
+// Function to insert random user data into directory-cards and modal-cards
+function insertRandomUsers() {
+  // Get properties from JSON Object
+  // let sourceImage = `${data.picture.large}`;
+  // let sourceName = `${data.name.first} ${data.name.last}`;
+  // let sourceEmail = data.email;
+  // let sourceCity = data.location.city;
+  // let sourcePhone = data.phone;
+  // let parsedPhone = `tel:+1${parsePhoneNumber(data.phone)}`;
+  // let sourceAddress = `${data.location.street}, ${data.location.city}, ${data.location.state} ${data.location.postcode}`;
+  // let sourceBirthday = data.email;
+
+  //For each random user... (may use map())
+  // data.map( user => {
+  insertBasicInfo($dcImage, $dcName, $dcEmail, $dcCity);
+  insertModalInfo();
+
+
+  // });
 
 }
 
-// Function for populating modal-card
 
+// Function for inserting basic employee info
+  // variables to pass in: image, name, email, city
+  // ... I may also need to pass in the object properties from the JSON object, perhaps as "sources".
+function insertBasicInfo (imageTarget, nameTarget,  emailTarget, cityTarget) {
+  // Get properties from JSON Object
+  // let sourceImage = `${data.picture.large}`;
+  // let sourceName = `${data.name.first} ${data.name.last}`;
+  // let sourceEmail = data.email;
+  // let sourceCity = data.location.city;
+  // let sourcePhone = data.phone;
+  // let parsedPhone = `tel:+1${parsePhoneNumber(data.phone)}`;
+  // let sourceAddress = `${data.location.street}, ${data.location.city}, ${data.location.state} ${data.location.postcode}`;
+  // let sourceBirthday = data.email;
+
+  // Insert employee image
+  imageTarget.attr('src', `${sourceImage}`);
+  // Insert First and Name
+  nameTarget.text(`${sourceName}`);
+  // Insert email
+  emailTarget.text(`${sourceEmail}`);
+  // Insert city
+  cityTarget.text(`${sourceCity}`);
+} // end of insertBasicInfo()
+
+// Function for populating modal-card
+function insertModalInfo() {
   // Insert basic info
+  insertBasicInfo($mcImage, $mcName, $mcEmail, $mcCity);
 
   // Insert email into link w/ mailto
-
+  $mcEmail.attr('href', `mailto:${sourceEmail}`);
   // Insert cell number
-
+  $mcPhone.text(`${sourcePhone}`);
   // Insert parsed phone number into link w/ tel
-
+  $mcPhone.attr('href', `${parsedPhone}`);
   // Insert address
-
+  $mcAddress.text(`${sourceAddress}`);
   // Insert birthday
-
-// Function to capitalize employee first and last name
+  $mcBirthday.text(`${sourceBirthday}`);
+} // end of insertModalInfo()
 
 // Function for parsing phone number
+function parsePhoneNumber(phoneString) {
+  // Split phone number string into an array of individual string characters
+  const splitNum = phoneString.split('');
+  // Create array in which to store just numbers/integers from phone number string
+  const intArray = [];
+
+  // Return only the characters that are integers
+  splitNum.forEach(char => {
+    if (Number.isInteger(parseInt(char))) {
+      intArray.push(parseInt(char));
+    }
+  });
+
+  // Return the joined numbers as one long integer
+  const parsedNumber = parseInt(intArray.join(''));
+
+  // return new phone number to be added into new array from map()
+  return parsedNumber;
+} // end of parsePhoneNumber
+
+// Function to rearrange numbers for user birthday
+function formatBirthday(birthday) {
+  const year = birthday.substring(0, 4);
+  const month = birthday.substring(5, 7);
+  const day = birthday.substring(8, 10);
+
+  return `${month}/${day}/${year}`;
+}
+
+
+// 09-27-2018: I only realized in the afternoon, after working on this function for an hour that the CSS property value text-transform: capitalize does what I need this function to do. I may complete this function later for fun.
+// Function to capitalize employee data
+function capitalize(dataString) {
+  // Capitalize the first letter in the data string
+  let cappedString = dataString;
+
+  // If first character is not a number...
+  if (isNaN(cappedString[0])) {
+    cappedString = dataString[0].toUpperCase() + dataString.substring(1);
+  }
+
+  // If string does not contain a space...
+  if (!cappedString.includes(' ')) {
+    //
+    return cappedString;
+  }
+  else {
+    // Split word(s) from data into an array of individual characters
+    const splitData = cappedString.split('');
+
+    // Loop to find letters to be capitalized
+    const multiCap =
+      // Loop over split data
+      splitData.forEach( char => {
+        // If the character before this one is a space
+        if (char.index() === ' ') {
+          // Capitalize this character
+          this.toUpperCase();
+
+        }
+
+      });
+      // Join characters back into a string
+      cappedString = multiCap.join('');
+      return cappedString;
+
+  } // end of else statement
+}
+
+// });
